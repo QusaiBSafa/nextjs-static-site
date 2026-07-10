c6bf921d188d23efe58ebc26b1b51aa32166cf99
Qusai Safa <153466377+QusaiBSafa@users.noreply.github.com>
2026-07-10 03:01:18 +0300

    Add src/pages/blog.tsx

---
+++ src/pages/blog.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blog: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Blog</h1>
+      <p className="mt-4">This is the blog page with static content.</p>
+    </div>
+  );
+};
+
+export default Blog;
